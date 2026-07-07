aaac7bc7df8bb3f4c3a009f7fb26a21f9c82f526
vitaliyplatonov95-crypto <vitaliyplatonov95@gmail.com>
2026-07-07 14:00:28 +0300

    Sprint 4.4: Load categories CSV into PostgresSQL

--- src/initial_load/csv_read_test.py
+++ src/initial_load/csv_read_test.py
@@ -1,13 +1,46 @@
 
+import psycopg
 import csv
 
 file_path = r"C:\Users\vital\OneDrive\Рабочий стол\coffee-data-platform\data\initial\categories.csv"
 
-def read_csv(file_path):
+connection = None
+
+try:
+    connection = psycopg.connect(
+        host='localhost',
+        port=5432,
+        dbname='coffee_data_platform',
+        user='postgres',
+        password='1234'
+    )
+
+    cursor = connection.cursor()
+
     with open(file_path, 'r', encoding='utf-8') as file:
         reader = csv.reader(file)
+
+        next(reader)
 
         for row in reader:
             print(row)
 
-read_csv(file_path)
+            cursor.execute(
+                """
+                INSERT INTO categories (category_id, category_name)
+                VALUES (%s, %s);
+                """,
+                (row[0], row[1])
+            )
+
+    connection.commit()
+
+except psycopg.OperationalError as e:
+    print(e)
+
+except FileNotFoundError:
+    print("Файл не найден")
+
+finally:
+    if connection:
+        connection.close()
