009a4a03ad3a7e7e8b74a38e5f2548edf9f4da2e
vitaliyplatonov95-crypto <vitaliyplatonov95@gmail.com>
2026-07-09 13:13:06 +0300

    Load stores CSV into PostgreSQL

---
+++ src/initial_load/load_stores.py
@@ -0,0 +1,46 @@
+
+import psycopg
+import csv
+
+file_path = r"C:\Users\vital\OneDrive\Рабочий стол\coffee-data-platform\data\initial\stores.csv"
+
+connection = None
+
+try:
+    connection = psycopg.connect(
+        host='localhost',
+        port=5432,
+        dbname='coffee_data_platform',
+        user='postgres',
+        password='1234'
+    )
+
+    cursor = connection.cursor()
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+
+        next(reader)
+
+        for row in reader:
+
+            cursor.execute(
+                """INSERT INTO stores (
+                    store_id,
+                    store_name,
+                    address,
+                    region,
+                    opening_date,
+                    is_active)
+                    VALUES (%s, %s, %s, %s, %s, %s)
+                    """,
+                (row[0], row[1], row[2], row[3], row[4], row[5])
+            )
+            connection.commit()
+except psycopg.OperationalError as error:
+    print(error)
+except FileNotFoundError as error:
+    print(error)
+finally:
+    if connection:
+        connection.close()
