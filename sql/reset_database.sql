1e74b27130ec6dfb8c2c1491935be92032f5534e
vitaliyplatonov95-crypto <vitaliyplatonov95@gmail.com>
2026-07-12 12:48:15 +0300

    Sprint 4.6: Add database reset script

---
+++ sql/reset_database.sql
@@ -0,0 +1,13 @@
+
+TRUNCATE TABLE
+    order_items,
+    payments,
+    deliveries,
+    orders,
+    employees,
+    products,
+    customers,
+    stores,
+    suppliers,
+    categories
+CASCADE;
