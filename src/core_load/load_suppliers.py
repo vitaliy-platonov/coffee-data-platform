700ce1a4128fc667a49720c2ae1c6f87fc2e2146
vitaliyplatonov95-crypto <vitaliyplatonov95@gmail.com>
2026-07-23 14:23:40 +0300

    Sprint 8: implement suppliers dimension ETL

---
+++ src/core_load/load_suppliers.py
@@ -0,0 +1,78 @@
+
+from database import get_connection
+import logging
+import logging_config
+
+def load_suppliers():
+    cursor = None
+    conn = None
+
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        inserted = 0
+        updated = 0
+
+        cursor.execute("""
+        SELECT
+            supplier_id,
+            supplier_name,
+            is_active
+        FROM staging.suppliers;""")
+
+        rows = cursor.fetchall()
+        logging.info(
+            f"Loaded {len(rows)} staging suppliers"
+        )
+
+        for row in rows:
+            cursor.execute("""
+            SELECT
+                supplier_key
+            FROM core.dim_suppliers
+            WHERE supplier_id = %s;""",
+                           (row[0],))
+
+            result = cursor.fetchone()
+
+            if result is None:
+                cursor.execute("""
+                INSERT INTO core.dim_suppliers 
+                (supplier_id, supplier_name, is_active)
+                VALUES (%s, %s, %s);""",
+                               (row[0], row[1], row[2]))
+                inserted += 1
+
+            else:
+                cursor.execute("""
+                UPDATE core.dim_suppliers
+                SET supplier_name = %s, 
+                is_active = %s,
+                updated_at = CURRENT_TIMESTAMP
+                WHERE supplier_id = %s;""",
+                               (
+                                   row[1],
+                                   row[2],
+                                   row[0]
+                               )
+                               )
+
+                updated += 1
+
+        conn.commit()
+        logging.info(f"Inserted: {inserted}")
+        logging.info(f"Updated: {updated}")
+    except Exception:
+        logging.exception("Load_suppliers failed")
+
+        if conn:
+            conn.rollback()
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+if __name__ == "__main__":
+    load_suppliers()
